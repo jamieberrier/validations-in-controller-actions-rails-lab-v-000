0026c2b9d43252d586d4f8beea2a862b43bdb405
jamieberrier <berrier.jamie@gmail.com>
2019-11-12 17:50:57 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    binding.pry
-    @post.update!(post_params)
+    if @post.update(post_params)
     redirect_to post_path(@post)
     #else
     #  render :edit
